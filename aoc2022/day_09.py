446af35b7b7650b5979ce4a2ee596ca93681bba5
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2022-12-10 17:20:19 +0100

    feat: day nine

--- aoc2022/day_09.py
+++ aoc2022/day_09.py
@@ -1,25 +1,184 @@
-import math
+from dataclasses import dataclass
+from enum import Enum
 
-def offset(val: int) -> int:
-    return +1 if val > 1 else -1 if val < -1 else 0
+Point = tuple[int, int]
 
-def chessboard_distance(x1: int, y1: int, x2: int, y2: int) -> int:
-    distance = max(abs(x2 - x1), abs(y2 - y1)) 
-    return 0 if distance == 0 else distance - 1 # I just need the number of distance "steps" not the numerical distance
 
-def compensating_movement(head_x: int, head_y: int, tail_x: int, tail_y: int) -> tuple[int, int]:
-    distance = chessboard_distance(head_x, head_y, tail_x, tail_y)
+class MovementDirection(Enum):
+    LEFT = 0
+    RIGHT = 1
+    UP = 2
+    DOWN = 3
+
+
+_FILE_DIRECTION_TO_MOVEMENT_DIRECTION = {
+    "U": MovementDirection.UP,
+    "D": MovementDirection.DOWN,
+    "L": MovementDirection.LEFT,
+    "R": MovementDirection.RIGHT,
+}
+
+
+@dataclass
+class Movement:
+    direction: MovementDirection
+    steps: int
+
+    def vector(self) -> Point:
+        match self.direction:
+            case MovementDirection.UP:
+                return (0, -1)
+            case MovementDirection.DOWN:
+                return (0, 1)
+            case MovementDirection.LEFT:
+                return (-1, 0)
+            case MovementDirection.RIGHT:
+                return (1, 0)
+
+        return (0, 0)
+
+
+def movement_offset(coordinate_diff: int) -> int:
+    return +1 if coordinate_diff > 1 else -1 if coordinate_diff < -1 else 0
+
+
+def chessboard_distance(head: Point, tail: Point) -> int:
+    head_x, head_y = head
+    tail_x, tail_y = tail
+
+    distance = max(abs(tail_x - head_x), abs(tail_y - head_y))
+
+    return (
+        0 if distance == 0 else distance - 1
+    )  # I just need the number of distance "steps" not the numerical distance
+
+
+def compensating_movement(head: Point, tail: Point) -> Point:
+    distance = chessboard_distance(head, tail)
 
     if distance == 0:
         return (0, 0)
 
-    x_diff = head_x - tail_x        
+    head_x, head_y = head
+    tail_x, tail_y = tail
+
+    x_diff = head_x - tail_x
     y_diff = head_y - tail_y
 
-    offset_x = offset(x_diff)
-    offset_y = offset(y_diff)
+    offset_x = movement_offset(x_diff)
+    offset_y = movement_offset(y_diff)
 
     return (x_diff - offset_x, y_diff - offset_y)
 
-def transform(x: int, y: int, ) -> tuple[int, int]:
-    return
+
+def translate_point(movement_vector: Point, point: Point) -> Point:
+    point_x, point_y = point
+    movement_x, movement_y = movement_vector
+
+    return (point_x + movement_x, point_y + movement_y)
+
+
+def follow_head(head: Point, tail: Point) -> Point:
+    tail_compensating_movement = compensating_movement(head, tail)
+    return translate_point(tail_compensating_movement, tail)
+
+
+def apply_movement(movement: Movement, head: Point) -> Point:
+    return translate_point(movement.vector(), head)
+
+
+def read_movements_file(file_path: str) -> list[Movement]:
+    result = []
+
+    with open(file_path, "r") as file:
+        for movement_row in file:
+            if not movement_row.isspace():
+                direction, steps = movement_row.strip().split(" ")
+
+                movement = Movement(
+                    _FILE_DIRECTION_TO_MOVEMENT_DIRECTION[direction], int(steps)
+                )
+
+                result.append(movement)
+
+    return result
+
+
+def part_one(movements: list[Movement]) -> int:
+    head = (0, 0)
+    tail = (0, 0)
+
+    unique_tail_positions = {tail}
+
+    for movement in movements:
+        for _ in range(0, movement.steps):
+            head = apply_movement(movement, head)
+            tail = follow_head(head, tail)
+
+            unique_tail_positions.add(tail)
+
+    return len(unique_tail_positions)
+
+
+_ROPE_SEGMENTS = 10
+_HEAD_SEGMENT_INDEX = 0
+_TAIL_SEGMENT_INDEX = 9
+
+
+def part_two(movements: list[Movement]) -> int:
+    rope_segments = [(0, 0)] * _ROPE_SEGMENTS
+
+    unique_tail_positions = {rope_segments[_TAIL_SEGMENT_INDEX]}
+
+    for movement in movements:
+        for _ in range(0, movement.steps):
+            rope_segments[_HEAD_SEGMENT_INDEX] = apply_movement(
+                movement, rope_segments[_HEAD_SEGMENT_INDEX]
+            )
+
+            for i in range(0, _ROPE_SEGMENTS - 1):
+                rope_segments[i + 1] = follow_head(
+                    rope_segments[i], rope_segments[i + 1]
+                )
+
+            unique_tail_positions.add(rope_segments[_TAIL_SEGMENT_INDEX])
+
+    minx = min(position[0] for position in unique_tail_positions)
+    maxx = max(position[0] for position in unique_tail_positions)
+    miny = min(position[1] for position in unique_tail_positions)
+    maxy = max(position[1] for position in unique_tail_positions)
+
+    print(minx, maxx)
+
+    for y in range(miny, maxy + 1):
+        for x in range(minx, maxx + 1):
+            if (x, y) == (0, 0):
+                print('s', end='')
+            elif (x, y) == rope_segments[_HEAD_SEGMENT_INDEX]:
+                print('H', end='')
+            elif (x, y) == rope_segments[_TAIL_SEGMENT_INDEX]:
+                print('T', end='')
+            elif (x, y) == rope_segments[1]:
+                print('1', end='')
+            elif (x, y) == rope_segments[2]:
+                print('2', end='')
+            elif (x, y) == rope_segments[3]:
+                print('3', end='')
+            elif (x, y) == rope_segments[4]:
+                print('4', end='')
+            elif (x, y) == rope_segments[5]:
+                print('5', end='')
+            elif (x, y) == rope_segments[6]:
+                print('6', end='')
+            elif (x, y) == rope_segments[7]:
+                print('7', end='')
+            elif (x, y) == rope_segments[8]:
+                print('8', end='')
+            elif (x, y) in unique_tail_positions:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
+
+
+    return len(unique_tail_positions)
--- tests/test_day_09.py
+++ tests/test_day_09.py
@@ -1,75 +1,166 @@
 import unittest
 
-from aoc2022.day_09 import compensating_movement
+from aoc2022.day_09 import (
+    Movement,
+    MovementDirection,
+    apply_movement,
+    follow_head,
+    compensating_movement,
+    part_one,
+    part_two,
+    read_movements_file,
+)
 
 
 class DayNineTestCase(unittest.TestCase):
-    def test_tail_follow_head_1(self):
-        # .....
-        # .TH..
-        # .....
+    def test_tail_follows_head_1(self):
+        # .....    .....    .....
+        # .....    ..H..    ..H..
+        # ..H.. -> ..... -> ..T..
+        # .T...    .T...    .....
+        # .....    .....    .....
 
-        movement = compensating_movement(3, 2, 2, 2)
+        head = (3, 3)
+        tail = (2, 4)
 
-        self.assertEqual((0, 0), movement) # Tail does not move
+        head = apply_movement(Movement(MovementDirection.UP, 1), head)
+        tail = follow_head(head, tail)
 
-    def test_tail_follow_head_4(self):
-        # ....
-        # .H..
-        # ..T.
-        # ....
+        self.assertEqual(head, (3, 2))
+        self.assertEqual(tail, (3, 3))
 
-        movement = compensating_movement(2, 2, 3, 3)
+    def test_tail_follows_head_2(self):
+        # .....    .....    .....
+        # .....    .....    .....
+        # ..H.. -> ...H. -> ..TH.
+        # .T...    .T...    .....
+        # .....    .....    .....
 
-        self.assertEqual((0, 0), movement) # Tail does not move
+        head = (3, 3)
+        tail = (2, 4)
 
-    def test_tail_follow_head_4(self):
-        # ...
-        # .H. (H covers T)
-        # ...
+        head = apply_movement(Movement(MovementDirection.RIGHT, 1), head)
+        tail = follow_head(head, tail)
 
-        movement = compensating_movement(2, 2, 2, 2)
+        self.assertEqual(head, (4, 3))
+        self.assertEqual(tail, (3, 3))
 
-        self.assertEqual((0, 0), movement) # Tail does not move
+    def test_tail_follows_head_3(self):
+        # ...    ...    ...
+        # .T.    .T.    ...
+        # .H. -> ... -> .T.
+        # ...    .H.    .H.
+        # ...    ...    ...
 
-    def test_tail_follow_head_4(self):
+        head = (2, 3)
+        tail = (2, 2)
+
+        head = apply_movement(Movement(MovementDirection.DOWN, 1), head)
+        tail = follow_head(head, tail)
+
+        self.assertEqual(head, (2, 4))
+        self.assertEqual(tail, (2, 3))
+
+    def test_tail_follows_head_3(self):
+        # .....    .....    .....
+        # .TH.. -> .T.H. -> ..TH.
+        # .....    .....    .....
+
+        head = (3, 2)
+        tail = (2, 2)
+
+        head = apply_movement(Movement(MovementDirection.RIGHT, 1), head)
+        tail = follow_head(head, tail)
+
+        self.assertEqual(head, (4, 2))
+        self.assertEqual(tail, (3, 2))
+
+    def test_tail_stands_still_1(self):
         # .....    .....
-        # .T.H. -> ..TH.
+        # .TH.. -> .T...
+        # .....    ..H..
+
+        head = (3, 2)
+        tail = (2, 2)
+
+        head = apply_movement(Movement(MovementDirection.DOWN, 1), head)
+        tail = follow_head(head, tail)
+
+        self.assertEqual(head, (3, 3))
+        self.assertEqual(tail, (2, 2))
+
+    def test_tail_stands_still_1(self):
+        # .....    .....
+        # .TH.. -> .H... (H covers T)
         # .....    .....
 
-        movement = compensating_movement(4, 2, 2, 2)
+        head = (3, 2)
+        tail = (2, 2)
 
-        self.assertEqual((1, 0), movement) # Tail moves one step to the right
+        head = apply_movement(Movement(MovementDirection.LEFT, 1), head)
+        tail = follow_head(head, tail)
 
-    def test_tail_follow_head_5(self):
-        # ...    ...
-        # .T.    ...
-        # ... -> .T.
-        # .H.    .H.
-        # ...    ...
+        self.assertEqual(head, (2, 2))
+        self.assertEqual(tail, (2, 2))
 
-        movement = compensating_movement(2, 4, 2, 2)
+    def test_part_one_solves_example_input(self):
+        result = part_one(
+            [
+                Movement(MovementDirection.RIGHT, 4),
+                Movement(MovementDirection.UP, 4),
+                Movement(MovementDirection.LEFT, 3),
+                Movement(MovementDirection.DOWN, 1),
+                Movement(MovementDirection.RIGHT, 4),
+                Movement(MovementDirection.DOWN, 1),
+                Movement(MovementDirection.LEFT, 5),
+                Movement(MovementDirection.RIGHT, 2),
+            ]
+        )
 
-        self.assertEqual((0, 1), movement) # Tail moves one step down
+        self.assertEqual(13, result)
 
-    def test_tail_follow_head_6(self):
-        # .....    .....
-        # ..H..    ..H..
-        # ..... -> ..T..
-        # .T...    .....
-        # .....    .....
+    def test_part_one_solves_real_input(self):
+        movements = read_movements_file("inputs/day_09.txt")
 
-        movement = compensating_movement(3, 2, 2, 4)
+        result = part_one(movements)
 
-        self.assertEqual((1, -1), movement) # Tail moves one step up and one step right
+        self.assertEqual(6018, result)
 
-    def test_tail_follow_head_7(self):
-        # .....    .....
-        # .....    .....
-        # ...H. -> ..TH.
-        # .T...    .....
-        # .....    .....
+    def test_part_two_solves_example_input_1(self):
+        result = part_two(
+            [
+                Movement(MovementDirection.RIGHT, 4),
+                Movement(MovementDirection.UP, 4),
+                Movement(MovementDirection.LEFT, 3),
+                Movement(MovementDirection.DOWN, 1),
+                Movement(MovementDirection.RIGHT, 4),
+                Movement(MovementDirection.DOWN, 1),
+                Movement(MovementDirection.LEFT, 5),
+                Movement(MovementDirection.RIGHT, 2),
+            ]
+        )
 
-        movement = compensating_movement(4, 3, 2, 4)
+        self.assertEqual(1, result)
 
-        self.assertEqual((1, -1), movement) # Tail moves one step up and one step right
+    def test_part_two_solves_example_input_2(self):
+        result = part_two(
+            [
+                Movement(MovementDirection.RIGHT, 5),
+                Movement(MovementDirection.UP, 8),
+                Movement(MovementDirection.LEFT, 8),
+                Movement(MovementDirection.DOWN, 3),
+                Movement(MovementDirection.RIGHT, 17),
+                Movement(MovementDirection.DOWN, 10),
+                Movement(MovementDirection.LEFT, 25),
+                Movement(MovementDirection.UP, 20),
+            ]
+        )
+
+        self.assertEqual(36, result)
+
+    def test_part_two_solves_real_input(self):
+        movements = read_movements_file("inputs/day_09.txt")
+
+        result = part_two(movements)
+
+        self.assertEqual(6018, result)
